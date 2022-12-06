da7667b5c7b1a5668412052d819742eda57b3232
4u.real.off <dm.churilov@gmail.com>
2022-12-06 18:50:27 +0300

    courses vc loaded with info

--- Networking/CoursesViewController.swift
+++ Networking/CoursesViewController.swift
@@ -9,10 +9,12 @@ import UIKit
 
 class CoursesViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+    var courses = [Course]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Courses"
+        tableView.delegate = self
         fetchData()
     }
     
@@ -23,17 +25,59 @@ class CoursesViewController: UIViewController {
         let session = URLSession.shared
         session.dataTask(with: url) { data, response, error in
             guard let data = data else { return }
-            let jsonDecoder = JSONDecoder()
             
             do {
-                let courses = try jsonDecoder.decode([Course].self, from: data)
-                print(courses)
+                let jsonDecoder = JSONDecoder()
+                jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+                self.courses = try jsonDecoder.decode([Course].self, from: data)
+               
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
             } catch let error {
                 print("Error serialization json", error)
             }
         }.resume()
     }
     
+    private func configureCell(_ cell: CourseCell, forIndexPath indexPath: IndexPath) {
+        let course = courses[indexPath.row]
+        cell.courseNameLabel .text = course.name
+        
+        if let numberOfLessons = course.numberOfLessons,
+           let numberOfTests = course.numberOfTests {
+            cell.numberOfLessonsLabel.text = "Number of lessons: \(numberOfLessons)"
+            cell.numberOfTestsLabel.text = "Number of tests: \(numberOfTests)"
+            
+            DispatchQueue.global().async {
+                guard let imageUrl = URL(string: course.imageUrl) else { return }
+                guard let imageData = try? Data(contentsOf: imageUrl) else { return }
+   
+                DispatchQueue.main.async {
+                    cell.picture.image = UIImage(data: imageData)
+                    
+                }
+            }
+        }
+    }
+}
 
+//MARK: Table view data source
 
+extension CoursesViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        courses.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CourseCell
+        configureCell(cell, forIndexPath: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+    
 }
---
+++ Networking/Models/CourseCell.swift
@@ -0,0 +1,15 @@
+//
+//  CourseCell.swift
+//  Networking
+//
+//  Created by CHURILOV DMITRIY on 06.12.2022.
+//
+
+import UIKit
+
+class CourseCell: UITableViewCell {
+    @IBOutlet var picture: UIImageView!
+    @IBOutlet var courseNameLabel: UILabel!
+    @IBOutlet var numberOfLessonsLabel: UILabel!
+    @IBOutlet var numberOfTestsLabel: UILabel!
+}
