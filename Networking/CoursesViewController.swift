0d28627ebd0a9cdee3b09526f8821ef7e98d2940
4u.real.off <dm.churilov@gmail.com>
2022-12-06 12:04:38 +0300

    Courses table vc with layout is created

---
+++ Networking/CoursesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CoursesViewController.swift
+//  Networking
+//
+//  Created by CHURILOV DMITRIY on 06.12.2022.
+//
+
+import UIKit
+
+class CoursesViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Courses"
+    }
+    
+
+
+}
